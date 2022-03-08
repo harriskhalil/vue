88c228083e0dadc7a6d2518fbf7f63628d3d38b4
Haris Muhammed <hm@dicube.at>
2022-03-08 15:11:35 +0500

    attribute bindind

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image_data:{
+                image:'./assets/images/socks_blue.jpg',
+                url:'https://vuejs.org/'
+            }
         }
     }
 })

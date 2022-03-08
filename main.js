f1672f425e6299db9c331a9b222f05b1a8016c3e
Haris Muhammed <hm@dicube.at>
2022-03-08 16:15:58 +0500

    Event Handeling

--- main.js
+++ main.js
@@ -11,11 +11,26 @@ const app = Vue.createApp({
                 details:['50% cotton','30% wool','20% wool'],
                 variants:[
 
-                    { id:3 , color:'green' },
-                    { id:4 , color:'blue' },
+                    { id:3 , color:'green', image:'./assets/images/socks_green.jpg' },
+                    { id:4 , color:'blue', image:'./assets/images/socks_blue.jpg' },
                 ],
                 sizes:['xs','sm','md','xl','lg']
             },
+            cart:0
         }
     },
+    methods:{
+        updatecart(){
+            this.cart+=1
+        },
+        minuscart(){
+            if (this.cart>0){
+
+                this.cart -=1
+            }
+        },
+        updateimage(variantImage){
+            this.image_data.image=variantImage
+        }
+    }
 })

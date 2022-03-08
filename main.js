6c8e21f7fe180be5e88c23e274b6fbce1acbeb43
Haris Muhammed <hm@dicube.at>
2022-03-08 15:57:52 +0500

    List Renderin using v-for

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ const app = Vue.createApp({
                 url:'https://vuejs.org/',
                 instock: true,
                 inventory:100,
-                onsale:true
-            }
+                onsale:true,
+                details:['50% cotton','30% wool','20% wool'],
+                variants:[
+
+                    { id:3 , color:'green' },
+                    { id:4 , color:'blue' },
+                ],
+                sizes:['xs','sm','md','xl','lg']
+            },
         }
-    }
+    },
 })
